da3e9e6fba2086f052f17462e843abe610f7c6b4
Piero Valdebenito <piero.valdebenito@schibsted.com>
2018-12-17 12:09:57 -0300

    [feat/error-control] - Added sendWorker upload & retry constants

--- pkg/domain/constants.go
+++ pkg/domain/constants.go
@@ -1,8 +1,13 @@
 package domain
 
 const (
+<<<<<<< HEAD
 	// SWUpload send worker is uploading images for first time
 	SWUpload = iota
+=======
+	// SWUplaod send worker is uploading images for first time
+	SWUplaod = iota
+>>>>>>> [feat/error-control] - Added sendWorker upload & retry constants
 	// SWRetry send worker retries to upload images because previous errors
 	SWRetry
 )
