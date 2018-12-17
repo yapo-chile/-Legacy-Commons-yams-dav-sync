129fd5155e2cfae48f4196f45569da88eddbaa9f
Piero Valdebenito <piero.valdebenito@schibsted.com>
2018-12-17 17:48:41 -0300

    [remove-redis] - Fix domain.constants.go file

--- pkg/domain/constants.go
+++ pkg/domain/constants.go
@@ -1,13 +1,8 @@
 package domain
 
 const (
-<<<<<<< HEAD
 	// SWUpload send worker is uploading images for first time
 	SWUpload = iota
-=======
-	// SWUplaod send worker is uploading images for first time
-	SWUplaod = iota
->>>>>>> [feat/error-control] - Added sendWorker upload & retry constants
 	// SWRetry send worker retries to upload images because previous errors
 	SWRetry
 )
