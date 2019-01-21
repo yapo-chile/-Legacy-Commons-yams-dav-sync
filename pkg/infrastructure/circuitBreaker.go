c8d72de19d7ae9f79c686b40e4423a070324bff6
Piero Valdebenito <piero.valdebenito@schibsted.com>
2019-01-21 11:53:15 -0300

    [feat/circuit-breaker] - Fix in-one-line declaration & fix unit tests

--- pkg/infrastructure/circuitBreaker.go
+++ pkg/infrastructure/circuitBreaker.go
@@ -6,7 +6,23 @@ import (
 	"github.com/sony/gobreaker"
 )
 
-var circuitBreaker *gobreaker.CircuitBreaker
+// CircuitBreaker common variables
+var (
+	// StateClosed represents Circuit breaker closed state
+	StateClosed = gobreaker.StateClosed
+
+	// StateHalfOpen represents Circuit breaker halft-open state
+	StateHalfOpen = gobreaker.StateHalfOpen
+
+	// StateOpen represents Circuit breaker open state
+	StateOpen = gobreaker.StateOpen
+
+	// ErrTooManyRequests is returned when the CB state is half open and the requests count is over the cb maxRequests
+	ErrTooManyRequests = gobreaker.ErrTooManyRequests
+
+	// ErrOpenState is returned when the CB state is open
+	ErrOpenState = gobreaker.ErrOpenState
+)
 
 // NewCircuitBreaker initializes circuit breaker wrapper
 // name is the circuit breaker
@@ -16,33 +32,20 @@ var circuitBreaker *gobreaker.CircuitBreaker
 // If Interval is 0, the CircuitBreaker doesn't clear internal Counts during the closed state.
 // Timeout is the period of the open state, after which the state of the CircuitBreaker becomes half-open.
 func NewCircuitBreaker(name string, consecutiveFailures uint32, failureRatio float64, timeout, interval int) CircuitBreaker {
-	var st gobreaker.Settings
-	st.Name = name
-	// If ReadyToTrip returns true, the CircuitBreaker will be placed into the open state
-	st.ReadyToTrip = func(counts gobreaker.Counts) bool {
-		errorRatio := float64(counts.TotalFailures) / float64(counts.Requests)
-		return errorRatio >= failureRatio || counts.ConsecutiveFailures > consecutiveFailures
+	settings := gobreaker.Settings{
+		Name:     name,
+		Timeout:  time.Duration(timeout) * (time.Second),
+		Interval: time.Duration(interval) * (time.Second),
+
+		// If ReadyToTrip returns true, the CircuitBreaker will be placed into the open state
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			errorRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+			return errorRatio >= failureRatio || counts.ConsecutiveFailures > consecutiveFailures
+		},
 	}
-	st.Timeout = time.Duration(timeout) * (time.Second)
-	st.Interval = time.Duration(interval) * (time.Second)
-	return gobreaker.NewCircuitBreaker(st)
+
+	return gobreaker.NewCircuitBreaker(settings)
 }
-
-// StateClosed represents Circuit breaker closed state
-var StateClosed = gobreaker.StateClosed
-
-// StateHalfOpen represents Circuit breaker halft-open state
-var StateHalfOpen = gobreaker.StateHalfOpen
-
-// StateOpen represents Circuit breaker open state
-var StateOpen = gobreaker.StateOpen
-
-var (
-	// ErrTooManyRequests is returned when the CB state is half open and the requests count is over the cb maxRequests
-	ErrTooManyRequests = gobreaker.ErrTooManyRequests
-	// ErrOpenState is returned when the CB state is open
-	ErrOpenState = gobreaker.ErrOpenState
-)
 
 // CircuitBreaker allows circuit breaker operations
 type CircuitBreaker interface {
